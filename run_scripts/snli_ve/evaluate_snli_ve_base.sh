948ca70b6d8ef021f1749dd55905640c2fa2f794
Tangshengku <981373223@qq.com>
2022-08-09 00:52:06 -0400

    early exit with layer similarity

--- run_scripts/snli_ve/evaluate_snli_ve_base.sh
+++ run_scripts/snli_ve/evaluate_snli_ve_base.sh
@@ -10,7 +10,7 @@ bpe_dir=../../utils/BPE
 # dev or test
 split=$1
 
-data=../../dataset/snli_ve_data/snli_ve_${split}.tsv
+data=/data/snli_ve_data/snli_ve_${split}.tsv
 path=../../checkpoints/snli_ve_base_best.pt
 result_path=../../results/snli_ve
 selected_cols=0,2,3,4,5
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch --nproc
     --path=${path} \
     --user-dir=${user_dir} \
     --task=snli_ve \
-    --batch-size=8 \
+    --batch-size=1 \
     --log-format=simple --log-interval=10 \
     --seed=7 \
     --gen-subset=${split} \
